27faec2576b1512b36d9859547297d144cf17054
h1net <ben@freshdevs.com>
2024-10-13 21:14:59 +0100

    feat: fixing openapi use

--- src/app/api/generateImage/route.ts
+++ src/app/api/generateImage/route.ts
@@ -1,5 +1,5 @@
-import { Configuration, OpenAIApi } from 'openai'
 import { NextResponse } from 'next/server'
+import { OpenAIApi, Configuration } from 'openai'
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
